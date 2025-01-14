b238b5f577142fc211a3329bb72d2207d968168b
Diego Martinez <diegomart7@gmail.com>
2025-01-14 13:33:11 +0100

    added a put failed request

--- tests/PUT.spec.js
+++ tests/PUT.spec.js
@@ -19,3 +19,17 @@ const res = await request.put('/posts/1', {
         "userId": 1,
     })))
 });
+
+test ('Update user post that doesnt exist', async ({request}) => {
+    const res = await request.put('/posts/200', {
+        data: {
+            id:1,
+            title:'Existing post',
+            body:'This is a post',
+            userId:1,
+            }
+        })
+        expect(res.ok()).toBeFalsy();
+        expect(res.status()).toBe(500);
+        expect(res.statusText()).toEqual('Internal Server Error')
+    });
